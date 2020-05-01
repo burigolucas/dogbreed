161f766e1ddaf10be7193a3c56603e15597dd7dc
Lucas Norberto Burigo <burigolucas@gmail.com>
2020-05-01 15:24:39 +0200

    Refactorate DogDetector and HumanFaceDetector in detectors.py
    Combine detector classes in a single file.

--- dogclf/detectors.py
+++ dogclf/detectors.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 from tensorflow.keras.applications.resnet50 import ResNet50
@@ -6,6 +7,35 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from .utils import convert_path_to_tensor
 from . import session
 
+# Class for human face detection
+class HumanFaceDetector():
+    def __init__(self):
+
+        # pre-trained face detector
+        self.face_cascade = cv2.CascadeClassifier(
+            cv2.data.haarcascades + "haarcascade_frontalface_alt.xml"
+            )
+
+    # Implementation of face detection
+    def detect_faces(self,img_path):
+        '''
+        INPUT:
+        img_path    path of image
+
+        OUTPUT:
+        boolean     True if face detected
+
+        Description:
+        Returns ndarray with faces detected in image
+        '''
+
+        img = cv2.imread(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray)
+
+        return faces
+
+# Class for dog detection
 class DogDetector():
 
     def __init__(self):
